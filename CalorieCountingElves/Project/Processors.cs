ac1116408c76bdb857e8778b5ab9ef0ac9abeb32
Landan <LandanParker@gmail.com>
2022-12-05 01:29:23 -0600

    .

--- CalorieCountingElves/Project/Processors.cs
+++ CalorieCountingElves/Project/Processors.cs
@@ -8,6 +8,62 @@ using System.Threading.Tasks;
 namespace CalorieCountingElves.Project
 {
 
+    public class StackProcessor
+    {
+        public string InputString { get; set; }
+
+        public static string GetTopCrates(StackProcessor processor)
+        {
+            var map = new Dictionary<int, Stack<char>>();
+
+            var strings = processor.InputString.Split('\n');
+
+            var columns = strings[8].Select((e, i) => 
+            {
+                return (Value: e, Index: i); 
+            }).Where(e =>
+            {
+                var success =  int.TryParse(e.Value.ToString(), out var item);
+                return success;
+            }).Select((e,i) => (e,i)).ToDictionary((e) => e.i);
+
+            foreach(var line in strings[..8].Reverse())
+            {
+                foreach((var Key, var Value) in columns)
+                {
+                    var val = line[Value.e.Index];
+                    if (char.IsLetter(val) is false) continue;
+
+                    map.TryAdd(Key, new Stack<char>()); 
+                    var stack = map[Key];
+                    stack.Push(val);
+                }
+            }
+
+            var instructions = strings[9..].Select(e=>e.Split(' ').Select(e=>int.TryParse(e, out var i) && i > 0?i:0).Where(e=>e > 0).ToArray()).Where(e=>e.Count() > 0).Select(e=>e is [var Amount, var From, var To]?(Amount,From,To):default).ToArray();
+
+            foreach((var Amount, var From, var To) in instructions)
+            {
+                for(int i = 0; i < Amount; i++)
+                {
+                    map[To-1].Push(map[From-1].Pop());
+                }
+            }
+
+            IEnumerable<char> GetTopValues()
+            {
+                foreach(var m in map.ToArray().OrderBy(e => e.Key))
+                {
+                    yield return m.Value.Peek();
+                }
+            }
+
+            var topValues = map.ToArray().OrderBy(e => e.Key).Select(e => e.Value.TryPeek(out var c)?c:'_');
+
+            return String.Join("", topValues);
+        }
+    }
+
     public class JobProcessor
     {
         public string InputString { get; set; }
